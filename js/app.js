9321ed0d92b6390540882f45a116578363bba613
边钢 <macisi528@gmail.com>
2013-01-03 16:58:34 +0800

    使用backbone作为mvc框架

--- js/app.js
+++ js/app.js
@@ -1,9 +1,75 @@
-'use strict';
+"use strict";
 
-/* app module */
-angular.module('reader', ['reader.filters', 'reader.services', 'reader.directives']).
-    config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/result', {templateUrl: 'partials/searchResult.html', controller: MyCtrl1});
-        $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-//        $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+google.load("feeds", "1");
+
+$(function(){
+
+    /*添加订阅*/
+    var Subscribe = Backbone.Model.extend({
+
+        defaults: {
+            query: ""
+        },
+
+        initialize: function(){
+        }
+
+    });
+
+    var subscribe = new Subscribe;
+
+    var SubscribeView = Backbone.View.extend({
+
+        el: $("#subscribe"),
+
+        template: Handlebars.compile($("#subscribeList").html()),
+
+        events: {
+            "click input[type='submit']": "search",
+            "keypress input[type='search']": "search",
+            "click .close": "remove"
+        },
+
+        initialize: function(){
+            this.input = this.$("input[type='search']");
+            this.model.set("query", this.input.val());
+            this.result = {};
+            this.resultContainer = this.$el.find("#result");
+
+            _.bindAll()
+        },
+
+        search: function(event){
+            var _this = this;
+            if (event.type === "keypress" && event.keyCode !== 13) {
+                return;
+            }
+            this.update();
+            google.feeds.findFeeds(this.model.get("query"), function(result){
+                if (!result.error) {
+                    _this.result = result;
+                    _this.render();
+                }
+            });
+        },
+
+        /*更新query值*/
+        update: function(){
+            this.model.set("query", this.input.val());
+        },
+
+        render: function(){
+            this.resultContainer.html(this.template(this.result));
+        },
+
+        /*移除订阅搜索结果*/
+        remove: function(){
+            this.resultContainer.html("");
+        }
+
+    });
+
+    var subscribeView = new SubscribeView({model: subscribe});
+
+
+});
